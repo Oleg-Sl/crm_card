0a70f4c2392c4ac9cff3736ff8a27ed4db27da8b
Oleg <slepcov_oleg@mail.ru>
2024-02-05 22:04:07 +0700

    Added api methods

--- static-deal-card/static/scripts/bx24/api_batch.js
+++ static-deal-card/static/scripts/bx24/api_batch.js
@@ -16,61 +16,61 @@ export default class BatchMethods {
         return data?.result;
     }
 
-    async getDataForStart(smartNumber, dealId) {
-        const cmd = {
-            currentUser: 'user.current',
-            fieldsDealData: 'crm.deal.fields',
-            departments: 'department.get',
-            fieldsProductData: `crm.item.fields?entityTypeId=${smartNumber}`,
-            dealContacts: `crm.deal.contact.items.get?id=${dealId}`,
-        };
+//    async getDataForStart(smartNumber, dealId) {
+//        const cmd = {
+//            currentUser: 'user.current',
+//            fieldsDealData: 'crm.deal.fields',
+//            departments: 'department.get',
+//            fieldsProductData: `crm.item.fields?entityTypeId=${smartNumber}`,
+//            dealContacts: `crm.deal.contact.items.get?id=${dealId}`,
+//        };
+//
+//        let data = await this.bx24.callMethod("batch", {
+//            halt: 0,
+//            cmd: cmd
+//        });
+//
+//        return data?.result;
+//    }
 
-        let data = await this.bx24.callMethod("batch", {
-            halt: 0,
-            cmd: cmd
-        });
-
-        return data?.result;
-    }
-
-    async getDealAndProducts(smartNumber, dealId) {
-        let reqPackage = {
-            deal: [
-                "crm.deal.list",
-                {
-                    filter: { "ID": dealId }, 
-                    select: ["*", "UF_*"],
-                    start: -1,
-                }
-            ],
-            products: [
-                "crm.item.list",
-                {
-                    "entityTypeId": smartNumber,
-                    "filter": { "parentId2": dealId },
-                    "select": [
-                        "id",
-                        SMART_FIELDS.TITLE,
-                        SMART_FIELDS.COUNT_PIECES,
-                        SMART_FIELDS.TECHNOLOGY,
-                        SMART_FIELDS.FILM,
-                        SMART_FIELDS.LAMINATION,
-                        SMART_FIELDS.WIDTH_FILM,
-                        SMART_FIELDS.LINEAR_METER_PIECES,
-                        SMART_FIELDS.SQUARE_METER_PIECES,
-                        SMART_FIELDS.LINEAR_METER_TOTAL,
-                        SMART_FIELDS.SQUARE_METER_TOTAL,
-                        SMART_FIELDS.LINK_SRC,
-                        SMART_FIELDS.CLIENT_FILES,
-                        SMART_FIELDS.PREPRESS,
-                        SMART_FIELDS.COMMENT,
-                    ],
-                    start: -1,
-                }
-            ]
-        };
-
-        let response = await await bx24.batchMethod(reqPackage);
-        return response;
-    }
+//    async getDealAndProducts(smartNumber, dealId) {
+//        let reqPackage = {
+//            deal: [
+//                "crm.deal.list",
+//                {
+//                    filter: { "ID": dealId },
+//                    select: ["*", "UF_*"],
+//                    start: -1,
+//                }
+//            ],
+//            products: [
+//                "crm.item.list",
+//                {
+//                    "entityTypeId": smartNumber,
+//                    "filter": { "parentId2": dealId },
+//                    "select": [
+//                        "id",
+//                        SMART_FIELDS.TITLE,
+//                        SMART_FIELDS.COUNT_PIECES,
+//                        SMART_FIELDS.TECHNOLOGY,
+//                        SMART_FIELDS.FILM,
+//                        SMART_FIELDS.LAMINATION,
+//                        SMART_FIELDS.WIDTH_FILM,
+//                        SMART_FIELDS.LINEAR_METER_PIECES,
+//                        SMART_FIELDS.SQUARE_METER_PIECES,
+//                        SMART_FIELDS.LINEAR_METER_TOTAL,
+//                        SMART_FIELDS.SQUARE_METER_TOTAL,
+//                        SMART_FIELDS.LINK_SRC,
+//                        SMART_FIELDS.CLIENT_FILES,
+//                        SMART_FIELDS.PREPRESS,
+//                        SMART_FIELDS.COMMENT,
+//                    ],
+//                    start: -1,
+//                }
+//            ]
+//        };
+//
+//        let response = await await bx24.batchMethod(reqPackage);
+//        return response;
+//    }
 }
--- static-deal-card/static/scripts/bx24/api_smart_process.js
+++ static-deal-card/static/scripts/bx24/api_smart_process.js
@@ -1,4 +1,3 @@
-import { SMART_FIELDS, } from '../parameters.js';
 
 
 export default class SmartProcessMethods {
@@ -35,33 +34,33 @@ export default class SmartProcessMethods {
         return data.fields;
     }
 
-    async getList(smartNumber, dealId) {
-        const params = {
-            entityTypeId: smartNumber,
-            filter: { parentId2: dealId },
-            select: [
-                "id",
-                SMART_FIELDS.TITLE,
-                SMART_FIELDS.COUNT_PIECES,
-                SMART_FIELDS.TECHNOLOGY,
-                SMART_FIELDS.FILM,
-                SMART_FIELDS.LAMINATION,
-                SMART_FIELDS.WIDTH_FILM,
-                SMART_FIELDS.LINEAR_METER_PIECES,
-                SMART_FIELDS.SQUARE_METER_PIECES,
-                SMART_FIELDS.LINEAR_METER_TOTAL,
-                SMART_FIELDS.SQUARE_METER_TOTAL,
-                SMART_FIELDS.LINK_SRC,
-                SMART_FIELDS.CLIENT_FILES,
-                SMART_FIELDS.PREPRESS,
-                SMART_FIELDS.COMMENT
-            ],
-            start: -1
-        };
-
-        const data = await this.bx24.callMethod("crm.item.list", params);
-        return data.items;
-    }
+//    async getList(smartNumber, dealId) {
+//        const params = {
+//            entityTypeId: smartNumber,
+//            filter: { parentId2: dealId },
+//            select: [
+//                "id",
+//                SMART_FIELDS.TITLE,
+//                SMART_FIELDS.COUNT_PIECES,
+//                SMART_FIELDS.TECHNOLOGY,
+//                SMART_FIELDS.FILM,
+//                SMART_FIELDS.LAMINATION,
+//                SMART_FIELDS.WIDTH_FILM,
+//                SMART_FIELDS.LINEAR_METER_PIECES,
+//                SMART_FIELDS.SQUARE_METER_PIECES,
+//                SMART_FIELDS.LINEAR_METER_TOTAL,
+//                SMART_FIELDS.SQUARE_METER_TOTAL,
+//                SMART_FIELDS.LINK_SRC,
+//                SMART_FIELDS.CLIENT_FILES,
+//                SMART_FIELDS.PREPRESS,
+//                SMART_FIELDS.COMMENT
+//            ],
+//            start: -1
+//        };
+//
+//        const data = await this.bx24.callMethod("crm.item.list", params);
+//        return data.items;
+//    }
 
     async delete(smartNumber, smartId) {
         let data = await this.bx24.callMethod("crm.item.delete", {
