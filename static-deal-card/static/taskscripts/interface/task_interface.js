dfe2ce9db5227dc64e7dc5a186bbdcadc8818b96
Oleg <slepcov_oleg@mail.ru>
2024-04-15 21:48:30 +0700

    Changed task interface

--- static-deal-card/static/taskscripts/interface/task_interface.js
+++ static-deal-card/static/taskscripts/interface/task_interface.js
@@ -148,14 +148,12 @@ export class TaskAppInterface {
         contentHTML += this.templates.getSummaryHTML(this.dataManager.groupsData);
         this.container.innerHTML = contentHTML;
 
-        // this.templateColumns = this.templateColumns.map((el, index) => index < 2 ? el : el * scale);
-        // this.templateColumns[1] = newWidth;
-        const newTemplateColumns = this.templateColumns.map(column => parseInt(column));
-        // const sum = newTemplateColumns.reduce((acc, column) => acc + column, 0);
-        // newTemplateColumns[newTemplateColumns.length - 1] += totalWidth - sum;
-        for (const t of this.container.querySelectorAll("table")) {
-            t.style.gridTemplateColumns = newTemplateColumns.join('px ') + 'px';
-        }      
+        if (this.templateColumns && this.templateColumns.length > 0) {
+            const newTemplateColumns = this.templateColumns.map(column => parseInt(column));
+            for (const t of this.container.querySelectorAll("table")) {
+                t.style.gridTemplateColumns = newTemplateColumns.join('px ') + 'px';
+            }
+        }
     }
 
     // Методы для изменения данных и уведомления TaskManager
