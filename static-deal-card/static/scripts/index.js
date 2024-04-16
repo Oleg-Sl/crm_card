fde25142449e6ff652bca25f07c948cc8f624048
Oleg <slepcov_oleg@mail.ru>
2024-04-16 21:50:44 +0700

    Changed task interface

--- static-deal-card/static/scripts/index.js
+++ static-deal-card/static/scripts/index.js
@@ -182,14 +182,14 @@ class App {
                 }
 
                 if (Object.keys(batch).length > 0) {
-                    // const resBatch = await this.bx24.batch.call(batch);
-                    // console.log("resBatch = ", resBatch);
-                    const resBatch = await this.bx24.callBatchMethod(batch);
+                    const resBatch = await this.bx24.batch.call(batch);
                     console.log("resBatch = ", resBatch);
-                    if (resBatch?.result_error && resBatch?.result_error.length > 0) {
-                        alert(`При сохранении произошла ошибка: ${JSON.stringify(resBatch?.result_error)}`);
-                        // console.log("resBatch = ", resBatch?.result_error);
-                    }
+                    // const resBatch = await this.bx24.callBatchMethod(batch);
+                    // // console.log("resBatch = ", resBatch);
+                    // if (resBatch?.result_error && resBatch?.result_error.length > 0) {
+                    //     alert(`При сохранении произошла ошибка: ${JSON.stringify(resBatch?.result_error)}`);
+                    //     // console.log("resBatch = ", resBatch?.result_error);
+                    // }
                 }
 
                 spinner.classList.add('d-none');
