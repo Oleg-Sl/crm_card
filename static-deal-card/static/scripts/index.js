624d4f4f2f8e61fb1703f9cb0d5cbf791b245edc
Oleg <slepcov_oleg@mail.ru>
2024-04-16 21:55:20 +0700

    Changed task interface

--- static-deal-card/static/scripts/index.js
+++ static-deal-card/static/scripts/index.js
@@ -185,7 +185,7 @@ class App {
                     // const resBatch = await this.bx24.batch.call(batch);
                     // console.log("resBatch = ", resBatch);
                     const resBatch = await this.bx24.callBatchMethod(batch);
-                    // console.log("resBatch = ", resBatch);
+                    console.log("resBatch = ", resBatch);
                     if (resBatch?.result_error && resBatch?.result_error.length > 0) {
                         alert(`При сохранении произошла ошибка: ${JSON.stringify(resBatch?.result_error)}`);
                         // console.log("resBatch = ", resBatch?.result_error);
