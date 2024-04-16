773f94016c745c8eadb913eb5a23eb80a997552b
Julissa Marquis <julissam@si-d3.nsk.21-school.ru>
2024-04-16 15:20:06 +0700

    Aded console log

--- static-deal-card/static/scripts/task/data/data_product.js
+++ static-deal-card/static/scripts/task/data/data_product.js
@@ -7,7 +7,7 @@ import {
 
 
 const FIELDS_MONEY = ['designCost', 'installCost', 'dismantlingCost', 'businessTripCost', 'deliveryCostPerTime'];
-const FIELDS_MONEY_BX24 = [SP_PRODUCT_FIELDS.designCost, SP_PRODUCT_FIELDSinstallCost, SP_PRODUCT_FIELDS.dismantlingCost, SP_PRODUCT_FIELDS.businessTripCost, SP_PRODUCT_FIELDS.deliveryCostPerTime];
+const FIELDS_MONEY_BX24 = [SP_PRODUCT_FIELDS.designCost, SP_PRODUCT_FIELDS.installCost, SP_PRODUCT_FIELDS.dismantlingCost, SP_PRODUCT_FIELDS.businessTripCost, SP_PRODUCT_FIELDS.deliveryCostPerTime];
 
 
 export class Product {
