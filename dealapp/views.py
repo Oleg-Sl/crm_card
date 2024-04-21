0d33d2ebb9ac7f08f224c73a4c204f1d56d94e9f
Oleg <slepcov_oleg@mail.ru>
2024-04-21 21:18:04 +0700

    Fixed output table task

--- dealapp/views.py
+++ dealapp/views.py
@@ -3,16 +3,17 @@ from rest_framework import views
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
-import logging
-import json
-import os
+# import logging
+# import json
+# import os
 
 
-log_file = 'dealapp.log'
-log_dir = os.path.dirname(os.path.abspath(__file__))
-log_path = os.path.join(log_dir, log_file)
+# log_file = 'dealapp.log'
+# log_dir = os.path.dirname(os.path.abspath(__file__))
+# log_path = os.path.join(log_dir, log_file)
 
-logging.basicConfig(filename=log_path, level=logging.INFO)
+
+# logging.basicConfig(filename=log_path, level=logging.INFO)
 
 class InstallApiView(views.APIView):
     @xframe_options_exempt
@@ -24,7 +25,7 @@ class IndexApiView(views.APIView):
     @xframe_options_exempt
     def post(self, request):
         r = request.data.get("PLACEMENT_OPTIONS", [])
-        logging.info(json.dumps(request.data))
+        # logging.info(json.dumps(request.data))
 
         try:
             match = re.search(r'\d+', r)
@@ -45,11 +46,15 @@ class TaskAppIndexApiView(views.APIView):
     @xframe_options_exempt
     def post(self, request):
         r = request.data.get("PLACEMENT_OPTIONS", [])
-        logging.info(json.dumps(request.data))
+        placement = request.data.get("PLACEMENT", [])
+        # logging.info(json.dumps(request.data))
         try:
             match = re.search(r'\d+', r)
-            id_task = match.group(0) if match else None
-            data = {"id": id_task}
+            id_entity = match.group(0) if match else None
+            if placement == 'CRM_DEAL_DETAIL_TAB':
+                data = {"dealId": id_entity, "taskId": None}
+            else:
+                data = {"taskId": id_entity, "dealId": None}
             return render(request, 'tpz/index.html', context=data)
         except TypeError:
             return HttpResponse("Ошибка: Не найден идентификатор задачи", status=400)
